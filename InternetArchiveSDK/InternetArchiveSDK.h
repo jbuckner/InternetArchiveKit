0b3bfec2cae9b7c1b5985ff142d87c348d8bac27
Jason Buckner <jason@jasonbuckner.com>
2018-11-05 18:59:03 -0800

    Initial Commit

---
+++ InternetArchiveSDK/InternetArchiveSDK.h
@@ -0,0 +1,19 @@
+//
+//  InternetArchiveSDK.h
+//  InternetArchiveSDK
+//
+//  Created by Jason Buckner on 11/5/18.
+//  Copyright © 2018 Jason Buckner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for InternetArchiveSDK.
+FOUNDATION_EXPORT double InternetArchiveSDKVersionNumber;
+
+//! Project version string for InternetArchiveSDK.
+FOUNDATION_EXPORT const unsigned char InternetArchiveSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <InternetArchiveSDK/PublicHeader.h>
+
+
