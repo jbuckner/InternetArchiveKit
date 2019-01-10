0c80e733245d30e41241a0ba93f1de5015fe1d97
Jason Buckner <jason@jasonbuckner.com>
2019-01-09 20:20:13 -0800

    Add additional metadata fields

--- InternetArchiveSDK/Models/ItemMetadata.swift
+++ InternetArchiveSDK/Models/ItemMetadata.swift
@@ -24,23 +24,33 @@ extension InternetArchive {
     public let discs: ModelField<String>?
     public let format: ModelField<String>?
     public let hasMp3: ModelField<String>?
+    public let hidden: ModelField<Bool>?
     public let homepage: ModelField<URL>?
     public let identifier: String
     public let indexflag: ModelField<String>?
     public let isDark: ModelField<String>?
     public let itemSize: ModelField<Int>?
     public let lineage: ModelField<String>?
+    public let limflag: ModelField<String>?
     public let md5s: ModelField<String>?
     public let mediatype: ModelField<String>?
     public let month: ModelField<Int>?
     public let notes: ModelField<String>?
+    public let numericId: ModelField<Int>?
+    public let numRecentReviews: ModelField<Int>?
+    public let numTopBa: ModelField<Int>?
+    public let numTopDl: ModelField<Int>?
     public let pick: ModelField<String>?
     public let `public`: ModelField<String>?
     public let publicdate: ModelField<String>?
     public let publisher: ModelField<String>?
+    public let rights: ModelField<String>?
     public let runtime: ModelField<String>?
     public let shndiscs: ModelField<String>?
+    public let showSearchByYear: ModelField<Bool>?
+    public let showSearchByDate: ModelField<Bool>?
     public let source: ModelField<String>?
+    public let spotlightIdentifier: ModelField<String>?
     public let subject: ModelField<String>?
     public let taper: ModelField<String>?
     public let tasks: ModelField<String>?
