efb6bb7ae60db184106057be616cab929d8d3e07
Jason Buckner <jason@jasonbuckner.com>
2019-04-16 20:51:07 -0700

    Update name field to be a String
    It is the primary key for a file so makes things easier not being an optional ModelField

--- InternetArchiveKit/Models/File.swift
+++ InternetArchiveKit/Models/File.swift
@@ -14,7 +14,19 @@ extension InternetArchive {
 
    This will be returned in the `files` property from an `InternetArchive().itemDetail()` request.
 
-   The properties are cast to their native Swift types (`Double`, `Int`, `TimeInterval`, `String`, etc)
+   **Note**: The properties are all type `ModelField<T>` **except** `name`, which is a `String`.
+   This means you need to access all values by their `.value` or `.values` properties, except for `identifier`,
+   which you can access directly.
+
+   **Some Background**: All other fields can be a string or array of strings so we can't access them
+   directly. See the `ModelField` class for a more thorough explanation.
+
+   For example:
+   ```
+   let file = File(...some file...)
+   file.name = "SCIRedRocksConcert.track1.mp3" // `name` is always a String, it's like the primary key for the file
+   file.length.value = TimeInterval object // we want to cast all other fields to their native type
+   ```
 
    See the Internet Archive's
    [Python API Reference](https://archive.org/services/docs/api/metadata-schema/index.html#file-metadata-schema)
@@ -32,8 +44,8 @@ extension InternetArchive {
     public let height: ModelField<IAInt>?
     public let length: ModelField<IATimeInterval>?
     public let md5: ModelField<IAString>?
-    public let mtime: ModelField<IAString>?
-    public let name: ModelField<IAString>?
+    public let mtime: ModelField<IAInt>?
+    public let name: String
     public let original: ModelField<IAString>?
     public let sha1: ModelField<IAString>?
     public let size: ModelField<IAInt>?
