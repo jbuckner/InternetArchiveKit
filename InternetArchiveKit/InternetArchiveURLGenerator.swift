b14a7749132446b3707cf4b738a4c929381e4243
Jason Buckner <jason@jasonbuckner.com>
2019-04-18 11:25:28 -0700

    Make URLGenerator init public

--- InternetArchiveKit/InternetArchiveURLGenerator.swift
+++ InternetArchiveKit/InternetArchiveURLGenerator.swift
@@ -10,7 +10,7 @@ import Foundation
 
 extension InternetArchive {
   public class URLGenerator: InternetArchiveURLGeneratorProtocol {
-    init(host: String = "archive.org", scheme: String = "https") {
+    public init(host: String = "archive.org", scheme: String = "https") {
       self.host = host
       self.scheme = scheme
     }
