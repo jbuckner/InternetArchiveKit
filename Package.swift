d6e5db34d89bbb4e4cf171fd29f694e855eb37a6
Jason Buckner <jason@jasonbuckner.com>
2020-11-12 18:25:01 -0800

    Add support for Swift PackageManager

---
+++ Package.swift
@@ -0,0 +1,19 @@
+// swift-tools-version:5.2
+import PackageDescription
+
+let package = Package(
+    name: "InternetArchiveKit",
+    platforms: [
+        .iOS(.v12)
+    ],
+    products: [
+        .library(name: "InternetArchiveKit", targets: ["InternetArchiveKit"])
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "InternetArchiveKit",
+            path: "InternetArchiveKit"
+        )
+    ]
+)
